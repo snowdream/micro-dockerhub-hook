3e3c4f99cce6b0c393b9115cedc33dcbeeb0f8f5
yanghui <snowdream@users.noreply.github.com>
2018-03-18 12:39:02 +0800

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   'maccyber/testhook': 'hello.sh parameter1 parameter2',
   'maccyber/maccyber.io': 'maccyber.io.sh',
-  'maccyber/tagtest:prod': 'tag.sh', // repo/image:tag
+  'snowdream/docker-xinyuntong:latest': 'tag.sh', // repo/image:tag
   'maccyber/fail': 'fail.sh'
 }
